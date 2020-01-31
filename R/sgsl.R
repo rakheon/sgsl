46a4f1a20b329c3c74ba8ab894a62b8142a4245f
rakheon <rakheon.kim@gmail.com>
2020-01-31 17:39:55 -0600

    rev

--- R/sgsl.R
+++ R/sgsl.R
@@ -1,3 +1,36 @@
+#' Fit the sparse group-subgroup lasso (SGSL)
+#'
+#' @param x p by N matrix of predictors (N: sample size, p: number of predictors)
+#' @param y 1 by N matrix of response variable
+#' @param type One of "lasso", "group", "ggroup", "ggroupind" or "sgsl".
+#' @param index.subgroup index for subgroups
+#' @param tau tau
+#' @param delta delta for cross-validation
+#' @param delta.group delta for cross-validation for groups
+#' @param delta.subgroup delta for cross-validation for subgroups
+#' @param delta.ind delta for cross-validation for ind
+#' @param standardize logical. TRUE for standardizing the data.
+#'
+#' @return out: indicators of the selected predictors. 1 for selected predictors and 0 for not selected predictors
+#' @export
+#'
+#' @examples
+#' tau=0.94
+#' set.seed(1)
+#' x <- matrix(rnorm(2400), nrow=80)
+#' y <- 0.25*x[1,] + 0.5*x[2,] + 0.75*x[3,] + 1.0*x[4,] + 0.25*x[5,] + 0.5*x[6,] + 0.75*x[7,] + 1.0*x[8,]  + matrix(rnorm(30), nrow=1)
+#' index.subgroup <- matrix(NA,nrow=10,ncol=80)
+#' index.subgroup[1,1:4]=1; index.subgroup[1,5:8]=2
+#' index.subgroup[2,9:12]=3; index.subgroup[2,13:16]=4
+#' index.subgroup[3,17:20]=5; index.subgroup[3,21:24]=6
+#' index.subgroup[4,25:28]=7; index.subgroup[4,29:32]=8
+#' index.subgroup[5,33:36]=9; index.subgroup[5,37:40]=10
+#' index.subgroup[6,41:44]=11; index.subgroup[6,45:48]=12
+#' index.subgroup[7,49:52]=13; index.subgroup[7,53:56]=14
+#' index.subgroup[8,57:60]=15; index.subgroup[8,61:64]=16
+#' index.subgroup[9,65:68]=17; index.subgroup[9,69:72]=18
+#' index.subgroup[10,73:76]=19; index.subgroup[10,77:80]=20
+#' sgslout <- sgsl(x,y,type="lasso",index.subgroup = index.subgroup)
 sgsl <- function(x,y,type=c("lasso", "group", "ggroup", "ggroupind", "sgsl")[1],
                  index.subgroup,#p.group=NULL,index=NULL,
                  #file.group="file",plots.ind=FALSE,plots.group=FALSE,plots.subgroup=FALSE,

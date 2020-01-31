7cbfcc225c9cc9b82e911f8a6ed037d6c3adf331
rakheon <rakheon.kim@gmail.com>
2020-01-31 11:28:22 -0600

    added descriptions

--- R/sgslasso.R
+++ R/sgslasso.R
@@ -1,40 +1,63 @@
+#' Fit the sparse group-subgroup lasso (SGSL)
+#'
+#' @param x p by N matrix of predictors (N: sample size, p: number of predictors)
+#' @param y 1 by N matrix of response variable
+#' @param type One of "lasso", "group", "ggroup", "ggroupind" or "sgsl".
+#' @param index index for groups
+#' @param index.subgroup index for subgroups
+#' @param p.group number of predictors in each group
+#' @param tau tau
+#' #@param file.group
+#' #@param plots.group
+#' #@param plots.subgroup
+#' #@param plots.ind
+#' @param delta delta for cross-validation
+#' @param delta.group delta for cross-validation for groups
+#' @param delta.subgroup delta for cross-validation for subgroups
+#' @param delta.ind delta for cross-validation for ind
+#' #@param use.Gram
+#' #@param format.data
+#' @param standardize logical. TRUE for standardizing the data.
+#'
+#' @return out: indicators of the selected predictors. 1 for selected predictors and 0 for not selected predictors
+#' @export
+#'
+#' @examples
 sgsl <- function(x,y,type=c("lasso", "group", "ggroup", "ggroupind", "sgsl")[1],
-                 index=NULL,index.subgroup=NULL,p.group=NULL,tau=NULL,
-                 file.group="file",plots.group=FALSE,plots.subgroup=FALSE,
-                 plots.ind=FALSE,delta.group=2,delta.subgroup=2,delta.ind=2,
-                 use.Gram=TRUE,format.data=FALSE,standardize=TRUE){
+                 index=NULL,index.subgroup=NULL,p.group=NULL,tau=NULL,plots.ind=FALSE,
+                 #file.group="file",plots.group=FALSE,plots.subgroup=FALSE,
+                 delta=2,delta.group=2,delta.subgroup=2,delta.ind=2,
+                 #use.Gram=TRUE,format.data=FALSE,
+                 standardize=TRUE){
 
     ## Methods ##
     if (type == "lasso"){
         ## Apply Lasso ##
         out <- lasso.computations(microbes=x,phenotypes=y,plots=FALSE,file="lasso_",
                                   format.data=FALSE,
-                                  delta=delta,use.Gram=use.Gram)$interest
+                                  delta=delta,use.Gram=TRUE)
     } else if (type == "group"){
         ## Apply pure group Lasso ##
         out <- pure.grplasso.computations(microbes=x,phenotypes=y,
                                           index=index,p.group=p.group,tau=tau,file.group="pure_group_lasso_",
-                                          plots.group=plots.group,delta.group=delta,
-                                          format.data=FALSE,standardize=standardize)$interest
+                                          plots.group=FALSE,delta.group=delta.group,
+                                          format.data=FALSE,standardize=standardize)
     } else if (type == "ggroup"){
         ## group group Lasso ##
         out <- group.group.lasso.computations(microbes=x,phenotypes=y,index,index.subgroup,p.group,tau=tau,
-                                              file.group="group_group_lasso_",plots.group=plots.group,plots.subgroup=plots.subgroup,
+                                              file.group="group_group_lasso_",plots.group=FALSE,plots.subgroup=FALSE,
                                               delta.group=2,delta.subgroup=2,format.data=FALSE,standardize=standardize)
     } else if (type == "ggroupind"){
         out <- group.group.indlasso.lasso.computations(microbes=x,phenotypes=y,index,index.subgroup,p.group,tau=tau,
-                                                       file.group="group_group_indlasso_",plots.group=plots.group,plots.subgroup=plots.subgroup,
-                                                       plots.ind=plots.ind,delta.group=2,delta.subgroup=2,delta.ind=2,
+                                                       file.group="group_group_indlasso_",plots.group=FALSE,plots.subgroup=FALSE,
+                                                       plots.ind=FALSE,delta.group=2,delta.subgroup=2,delta.ind=2,
                                                        use.Gram=TRUE,format.data=FALSE,standardize=standardize)
     } else if (type == "sgsl"){
         out <- sparse.group.subgroup.computations(microbes=x,phenotypes=y,group.index=index,subgroup.index=index.subgroup,
                                                   tau=tau,alpha1,alpha2,alpha3,nlam,lambdas,lambda.accuracy,
-                                                  file.group="sgsl_",plots.group=plots.group,delta.group=2,format.data=FALSE,
+                                                  file.group="sgsl_",plots.group=FALSE,delta.group=2,format.data=FALSE,
                                                   cv.criterion=FALSE,nfold=10,alphas.cv.range=seq(0.1,0.95,by=0.05))
     }
-
-
-
 
     return(out)
 
